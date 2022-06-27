c44cc8e792a2bbb2d5c47f692b4af375b72ed480
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-06-27 23:35:59 +0200

    Update url error

--- week4/urls.py
+++ week4/urls.py
@@ -17,7 +17,7 @@ import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.contrib.auth import views as auth_views
 from django.views.static import serve
 from django.views.generic import TemplateView
